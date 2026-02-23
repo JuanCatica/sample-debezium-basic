77889f82fd3930e26dfa0f8d5be5555a168f26a3
Juan Camilo Cática Buendía <juancatica.cmp@gmail.com>
2026-02-23 13:46:20 -0500

    fix

--- deployment/5-debezium-cdc/cdc.tf
+++ deployment/5-debezium-cdc/cdc.tf
@@ -206,7 +206,7 @@ resource "aws_iam_role_policy" "msk_connect_service" {
           "ssm:GetParameter",
           "ssm:GetParameters"
         ]
-        Resource = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/dbtester/*"
+        Resource = "arn:aws:ssm:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:parameter/dbtester/*"
       },
       # CloudWatch Logs
       {
